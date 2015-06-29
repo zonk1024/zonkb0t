1bfe714b716a32e44b16c78352ad91f9789355bd
Chris Jacobs <zonk1024@gmail.com>
2015-06-29 01:08:04 +0000

    file logging

--- logger.py
+++ logger.py
@@ -1,14 +1,39 @@
-import termcolor
+import os
 import time
+import atexit
+import settings
+import termcolor
+
+
+HOME_DIR = os.getenv('HOME')
+LOG_DIR = os.path.join(HOME_DIR, 'log')
+LOG_FILE = os.path.join(LOG_DIR, '{}.log'.format(settings.redis_prefix))
+if 'log' not in os.listdir(os.getenv('HOME')):
+    os.makedirs(LOG_DIR)
+LOG_FILE_OBJ = open(LOG_FILE, 'a')
+
+def close_log_file():
+    LOG_FILE_OBJ.close()
 
 def log(strings, colors):
-    out = '[{}] '.format(termcolor.colored(time.strftime('%Y-%m-%d %H:%M:%S'), 'cyan'))
+    t = time.strftime('%Y-%m-%d %H:%M:%S')
+    out = '[{}] '.format(termcolor.colored(t, 'cyan'))
+    out_simple = '[{}] '.format(t)
     colors = list(colors)
     while len(colors) % 2:
         colors.append(None)
     for s, c in zip(strings, colors):
         if c:
             out += termcolor.colored(s, c[0], **c[1])
+            try:
+                out_simple += s
+            except TypeError:
+                out_simple += repr(s)
         else:
             out += s
+            out_simple += s
     print out
+    LOG_FILE_OBJ.write('{}\n'.format(out_simple))
+    LOG_FILE_OBJ.flush()
+
+atexit.register(close_log_file)
